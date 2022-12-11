10a008ab6ff2d141ca2ea8e9672a7bb27c5a0a1c
AnikoShaverdashvili <shaverdashviliania@gmail.com>
2022-12-11 23:40:07 +0400

    Add project files.

---
+++ Models/Issue.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Issue
+    {
+
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+        public IssueType IssueTypeIssueType { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Complated { get; set; }
+    }
+    public enum Priority
+    {
+        Low,Medium,Hight
+    }
+    public enum IssueType
+    {
+        Feature,Bug,Documentation
+    }
+}
